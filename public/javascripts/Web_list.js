73c8378a66162a8a2dc3d936c8f02bcc1baae3d4
songsu73 <songsu73@naver.com>
2023-03-06 18:13:56 +0900

    전체서버표시, IP별로 표시할 수 있게 변경

--- public/javascripts/Web_list.js
+++ public/javascripts/Web_list.js
@@ -1,5 +1,10 @@
 let Lobis_web_list = null
 let xmlhttp = new XMLHttpRequest();
+var arr_HSDP_Client = [];
+var arr_101_ClientIP = [];
+var arr_102_ClientIP = [];
+var arr_103_ClientIP = [];
+var arr_104_ClientIP = [];
 
 xmlhttp.open("GET", "javascripts/Lobis_web.list",false);
 xmlhttp.send();
@@ -12,16 +17,57 @@ xmlhttp.send();
 if (xmlhttp.status == 200) { 
     HSDP_web_list = xmlhttp.responseText;
 }
-list_Lobis_web = Lobis_web_list.replace(/\n/gi,"\t")
+list_Lobis_web = Lobis_web_list.replace(/\n/g,"\t").replace(/\s/g,"\t")
 list_HSDP_web = HSDP_web_list.replace(/\n/gi,"\t")
 list_Lobis_web = list_Lobis_web.split('\t');
 list_HSDP_web  = list_HSDP_web.split('\t');
 
-document.write("<br>")
-// for (var i = 0; i < Lobis_web_list.length; i++) {
-//     document.write(Lobis_web_list[i] + "<br>")
-// }
+for (var i = 0; i < (list_HSDP_web.length/5); i++){
+    arr_HSDP_Client.push(list_HSDP_web[i*5+1])
+    let index = list_HSDP_web[i*5+3]
+    switch(index){
+        case '101' :
+            arr_101_ClientIP.push(list_HSDP_web[i*5+1]);
+            break;
+        case '102' :
+            arr_102_ClientIP.push(list_HSDP_web[i*5+1]);
+            break;
+        case '103' :
+            arr_103_ClientIP.push(list_HSDP_web[i*5+1]);
+            break;
+        case '104' :
+            arr_104_ClientIP.push(list_HSDP_web[i*5+1]);
+            break;
+    }
+}
+for (var i=0; i < (list_Lobis_web.length/7);i++){
+    arr_HSDP_Client.push(list_Lobis_web[i*7+1])
+    let index = list_Lobis_web[i*7+3]
+    switch(index){
+        case '101' :
+            arr_101_ClientIP.push(list_Lobis_web[i*7+1]);
+            break;
+        case '102' :
+            arr_102_ClientIP.push(list_Lobis_web[i*7+1]);
+            break;
+        case '103' :
+            arr_103_ClientIP.push(list_Lobis_web[i*7+1]);
+            break;
+        case '104' :
+            arr_104_ClientIP.push(list_Lobis_web[i*7+1]);
+            break;
+    }
+}
 
-// for (var i = 0; i < Lobis_web_list.length; i++) {
-//     document.write(HSDP_web_list[i] + "<br>")
-// }
+var set = new Set(arr_HSDP_Client);
+arr_HSDP_Client= [...set]; //- HSDP Client IP를 중복제거하고 담은 배열
+var set = new Set(arr_101_ClientIP);
+arr_101_ClientIP = [...set];
+var set = new Set(arr_102_ClientIP);
+arr_102_ClientIP = [...set];
+var set = new Set(arr_103_ClientIP);
+arr_103_ClientIP = [...set];
+var set = new Set(arr_104_ClientIP);
+arr_104_ClientIP = [...set];
+
+arr_HSDP_Client = arr_HSDP_Client.filter(x => x !== undefined);
